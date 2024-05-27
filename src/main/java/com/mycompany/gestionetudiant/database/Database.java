eb54c55b3f7bc0b07ab3d7f42355aa0c6e28d611
Mamadou Siradiou Diallo <mamadousiradioudiallo4@gmail.com>
2024-05-27 17:21:29 +0000

    Update database util

--- src/main/java/com/mycompany/gestionetudiant/database/Database.java
+++ src/main/java/com/mycompany/gestionetudiant/database/Database.java
@@ -1,4 +1,23 @@
 package com.mycompany.gestionetudiant.database;
 
-public class Database {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public abstract class Database {
+    private static final String url = "jdbc:mysql://localhost:3306/student_management";
+    private static final String userName = "root";
+    private static final String password = "eclipse23";
+    private static Connection connexion;
+
+    public static Connection getConnexion() throws SQLException {
+        connexion = DriverManager.getConnection(url, userName, password);
+
+        return connexion;
+    }
+
+    public static void close() throws SQLException {
+        connexion.close();
+    }
 }
+
