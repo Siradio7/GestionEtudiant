a10ea2edc1a543ff3a87a98d224f06fcacb86485
Mamadou Siradiou Diallo <mamadousiradioudiallo4@gmail.com>
2024-05-27 16:41:33 +0000

    Added Student model

---
+++ src/main/java/com/mycompany/gestionetudiant/model/Student.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.gestionetudiant.model;
+
+/**
+ *
+ * @author syntax-error
+ */
+
+public class Student {
+    private String name;
+    private String registrationNumber;
+    private String department;
+    private String sector;
+    private String level;
+    private String address;
+    private String phoneNumber;
+
+    public Student() {
+    }
+
+    public Student(String name, String registrationNumber, String department, String sector, String level, String address, String phoneNumber) {
+        this.name = name;
+        this.registrationNumber = registrationNumber;
+        this.department = department;
+        this.sector = sector;
+        this.level = level;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getSector() {
+        return sector;
+    }
+
+    public void setSector(String sector) {
+        this.sector = sector;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
